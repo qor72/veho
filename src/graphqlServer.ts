9e8496801e1b5e58124b77221abd60c4d7f88a0b
Al G <qor072@gmail.com>
2025-08-12 09:32:30 -0400

    LokiJS added

--- src/graphqlServer.ts
+++ src/graphqlServer.ts
@@ -2,6 +2,11 @@ const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
 const { schema } = require('./schema');
 const { root } = require('./resolvers');
+const Loki = require('lokijs');
+
+const db = new Loki('veho.db');
+const stowCollection = db.addCollection('stow');
+const inductCollection = db.addCollection('induct');
 
 const app = express();
 
